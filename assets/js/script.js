b47b8dca1b99c21e81ddcde721a3d201dc3a8958
joepa91 <85590718+joepa91@users.noreply.github.com>
2022-01-20 21:30:44 -0600

    pseudo coded css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // create formn where user inputs zipcode
-
 // create submit button to send zipcode
 // create function to obtain weather
 // based on weather send message with gif to determine whether or not umbrella is needed
